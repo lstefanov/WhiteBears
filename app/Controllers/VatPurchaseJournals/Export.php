23c97de0b0904fa060e7083805994c6a1cc53b29
Lyubomir Stefanov <igroup7@gmail.com>
2023-10-02 21:02:11 +0300

    Change dates

--- app/Controllers/VatPurchaseJournals/Export.php
+++ app/Controllers/VatPurchaseJournals/Export.php
@@ -54,7 +54,7 @@ class Export extends BaseController
     {
         $date = $this->request->getGet('date') ?? date('Y-m');
         $selectedBusinessId = $this->request->getGet('business_id') ?? 0;
-        $data = $this->generateData($selectedBusinessId, $date);
+        $data = $this->generateData($selectedBusinessId, $date, true);
 
         //Get business details
         $businessModel = new BusinessesModel();
@@ -68,6 +68,30 @@ class Export extends BaseController
         $sheet->fromArray($data, null, 'A3');
 
 
+        //format column A3 to A{last row} as text -> its a number but force it to text
+        $sheet->getStyle('A3:A' . (count($data) + 2))->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_NUMBER);
+        //format column B3 to B{last row} as text
+        $sheet->getStyle('B3:B' . (count($data) + 2))->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_TEXT);
+        //format column C3 to C{last row} as date
+        $sheet->getStyle('C3:C' . (count($data) + 2))->getNumberFormat()->setFormatCode('dd/mm/yyyy');
+        //format column D3 to D{last row} as date
+        $sheet->getStyle('D3:D' . (count($data) + 2))->getNumberFormat()->setFormatCode('dd/mm/yyyy');
+        //format column E3 to E{last row} as number
+        $sheet->getStyle('E3:E' . (count($data) + 2))->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_NUMBER_00);
+        //format column F3 to F{last row} as number
+        $sheet->getStyle('F3:F' . (count($data) + 2))->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_NUMBER_00);
+        //format column G3 to G{last row} as string
+        $sheet->getStyle('G3:G' . (count($data) + 2))->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_TEXT);
+        //format column H3 to H{last row} as string
+        $sheet->getStyle('H3:H' . (count($data) + 2))->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_TEXT);
+        //format column I3 to I{last row} as string
+        $sheet->getStyle('I3:I' . (count($data) + 2))->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_TEXT);
+        //format column J3 to J{last row} as string
+        $sheet->getStyle('J3:J' . (count($data) + 2))->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_TEXT);
+        //format column K3 to K{last row} as string
+        $sheet->getStyle('K3:K' . (count($data) + 2))->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_TEXT);
+
+
         $writer = new \PhpOffice\PhpSpreadsheet\Writer\Xlsx($spreadsheet);
         $writer->save($fileName);
 
@@ -78,7 +102,7 @@ class Export extends BaseController
     /**
      * @throws \Exception
      */
-    private function generateData(int $selectedBusinessId, string $date): array
+    private function generateData(int $selectedBusinessId, string $date, bool $forExport = false): array
     {
         $data = [];
 
@@ -87,6 +111,8 @@ class Export extends BaseController
         }
 
         $businessModel = new BusinessesModel();
+
+        $dateFormat = $forExport ? 'd/m/Y' : 'd-m-Y';
 
         //get last day of the month for param $date
         $last = new \DateTime($date);
@@ -105,17 +131,19 @@ class Export extends BaseController
         )->orderBy('invoice_date', 'asc')->findAll();
 
         foreach ($vpjFioniksFarmaEntities as $vpjFioniksFarmaEntity) {
-            $h_doc_type = '901';
-            if (strpos(mb_strtolower($vpjFioniksFarmaEntity['invoice_type']), 'корекционна') !== false) {
-                $h_doc_type = 904;
+            $h_doc_type = "901";
+            if (strpos(mb_strtolower($vpjFioniksFarmaEntity['invoice_type']), 'корекционна') !== false ||
+                strpos(mb_strtolower($vpjFioniksFarmaEntity['invoice_type']), 'финансов рабат') !== false ||
+                strpos(mb_strtolower($vpjFioniksFarmaEntity['invoice_type']), 'цен.разлики') !== false) {
+                $h_doc_type = '904';
             }
 
 
             $entity = [
                 'h_doc_type' => $h_doc_type,
                 'h_doc_no' => $vpjFioniksFarmaEntity['invoice'],
-                'h_doc_date' => date('d-m-Y', strtotime($vpjFioniksFarmaEntity['invoice_date'])),
-                'h_doc_valeur' => date('d-m-Y', strtotime($dateLastDayOfTheMonth)),
+                'h_doc_date' => date($dateFormat, strtotime($vpjFioniksFarmaEntity['invoice_date'])),
+                'h_doc_valeur' => date($dateFormat, strtotime($dateLastDayOfTheMonth)),
                 'd_net_value' => number_format(($vpjFioniksFarmaEntity['payment_summary'] * 100 / 120), 2, '.', ''),
                 'd_vat_value' => number_format(($vpjFioniksFarmaEntity['payment_summary'] * 20 / 120), 2, '.', ''),
                 'd_partner_type' => 11,
@@ -144,13 +172,13 @@ class Export extends BaseController
             }
 
             $h_doc_date = str_replace('г.', '', $vpjStingEntity['doc_date']);
-            $h_doc_date = date('d-m-Y', strtotime($h_doc_date));
+            $h_doc_date = date($dateFormat, strtotime($h_doc_date));
 
             $entity = [
                 'h_doc_type' => $h_doc_type,
                 'h_doc_no' => $vpjStingEntity['doc_n'],
                 'h_doc_date' => $h_doc_date,
-                'h_doc_valeur' => date('d-m-Y', strtotime($dateLastDayOfTheMonth)),
+                'h_doc_valeur' => date($dateFormat, strtotime($dateLastDayOfTheMonth)),
                 'd_net_value' => number_format(($vpjStingEntity['payment_summary'] * 100 / 120), 2, '.', ''),
                 'd_vat_value' => number_format(($vpjStingEntity['payment_summary'] * 20 / 120), 2, '.', ''),
                 'd_partner_type' => 11,
@@ -190,8 +218,8 @@ class Export extends BaseController
             $entity = [
                 'h_doc_type' => $h_doc_type,
                 'h_doc_no' => $vpjAsterEntity['invoice'],
-                'h_doc_date' => date('d-m-Y', strtotime($h_doc_date)),
-                'h_doc_valeur' => date('d-m-Y', strtotime($dateLastDayOfTheMonth)),
+                'h_doc_date' => date($dateFormat, strtotime($h_doc_date)),
+                'h_doc_valeur' => date($dateFormat, strtotime($dateLastDayOfTheMonth)),
                 'd_net_value' => number_format($vpjAsterEntity['price_without_vat'], 2, '.', ''),
                 'd_vat_value' => number_format($vpjAsterEntity['price_vat'], 2, '.', ''),
                 'd_partner_type' => 13,
